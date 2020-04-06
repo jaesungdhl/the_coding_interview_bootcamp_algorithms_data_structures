fb60cce7ad0c60029cec0a22bcab2d60ea9a7e59
Jaesung Lee <jaesungdhl@gmail.com>
2020-04-06 12:19:31 -0700

    finish string reversal section

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -10,11 +10,29 @@
 //     return str.split("").reverse().join("");
 // }
 
+// const reverse = str => {
+//     let newStr = "";
+//     for(let i = str.length - 1; i >= 0; i--) {
+//         newStr += str[i];
+//     }
+//     return newStr;
+// }
+
+// const reverse = str => {
+//     let newStr = "";
+//     debugger;
+//     for(let char of str) {
+//         newStr = char += newStr;
+//     }
+//     debugger;
+//     return newStr;
+// }
+
 const reverse = str => {
-    let newStr = "";
-    for(let i = str.length - 1; i >= 0; i--) {
-        newStr += str[i];
-    }
-    return newStr;
+    return str.split("").reduce((a,n) => {
+        a = n + a;
+        return a;
+    }, "")
 }
+
 module.exports = reverse;
